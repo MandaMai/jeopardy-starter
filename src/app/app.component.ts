6e6fad85abad2ab34a91feb6f636d1559fee6a48
MandaMai <a.maifeld@hotmail.com>
2017-09-19 11:34:42 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,42 @@ import {JeopardyService } from './jeopardy.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = 'Jeopardy';
 
   questionInfo;
+  userScore: number =0;
+  userAnswer: string;
 
   constructor(private jeopardyService: JeopardyService){}
 
+  // populating questionInfo 
   getDataFromService(){
     this.jeopardyService.getQuestionInfo()
       .subscribe(
         questionInfo => {
           this.questionInfo = questionInfo[0];
+          console.log(this.questionInfo.answer)
         }
       )
   }
 
+  checkAnswer() {
+    console.log(this.userAnswer)
+    if(this.userAnswer == this.questionInfo.answer) {
+      this.userScore += this.questionInfo.value;
+      this.userAnswer="";
+    }//update user score if answer is correct
+    this.getDataFromService();//get new question
+  }
+
+ 
+
   ngOnInit(){
+    // get question data when page is loaded
     this.getDataFromService()
   }
+
 
 }
